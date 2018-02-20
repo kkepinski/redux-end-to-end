0c5845a7f71d2a2c23d8508e78930a0f9c991a8c
Krzysztof Kepinski <Krzysztof.Kepinski@stepstone.com>
2018-02-20 13:45:14 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  entry: {
+    js: './index.js'
+  },
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: []
+  },
+  devtool: '#cheap-module-source-map',
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8080/',
+      open: true
+    })
+  ]
+}
